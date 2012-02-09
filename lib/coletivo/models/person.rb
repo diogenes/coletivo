f2389a22c51afebeb9cf39866eaeb69c593eafb5
Alex Smith <alex.smith@learnable.com>
2012-02-09 23:52:43 +1100

    Added a method to update an existing rating.

--- lib/coletivo/models/person.rb
+++ lib/coletivo/models/person.rb
@@ -20,6 +20,15 @@ module Coletivo
             :weight => weight
           })
         end
+
+        def rate_or_update!(rateable, weight)
+          r = Coletivo::Config.ratings_container.find_or_initialize_by_person_id_and_rateable_id_and_rateable_type(self.id, rateable.id, rateable.class)
+          r.person = self
+          r.rateable = rateable
+          r.weight = weight
+          r.save!
+          puts weight
+        end
       end # InstanceMethods
 
     end # Person
